df9f709638af73a7eb2881a00f6b088ee3ccab50
Hugo Eduardo Bueno Martinez <55301568+HugoEduardo10@users.noreply.github.com>
2019-10-01 20:31:30 -0500

    Add files via upload

---
+++ Nombre.py
@@ -0,0 +1,13 @@
+# Nombre.py
+# Autor: Hugo Bueno.
+# Fecha de creacion: 14/09/2019
+
+nombre=input("Nombre:")
+apellidos=input("Apellidos:")
+
+# Se concatan los valores str, junto con la literal " "
+nombreCompleto=nombre+" "+apellidos
+
+#nSe asigna a la variable la representacion en mayusculas de lo que contenia.
+nombreCompleto=nombreCompleto.upper()
+print(nombreCompleto) 
