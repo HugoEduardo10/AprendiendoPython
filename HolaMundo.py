0618e8d971aede82447b7f223d9aca3d0fc19514
Hugo Eduardo Bueno Martinez <55301568+HugoEduardo10@users.noreply.github.com>
2019-10-01 20:29:54 -0500

    Add files via upload

---
+++ HolaMundo.py
@@ -0,0 +1,9 @@
+# HolaMundo.py
+# Autor: Hugo Bueno
+# Fecha de creacion: 14/09/2019
+
+# Los comentarios se realizan con #. 
+
+# print genera una salida a consola. 
+
+print ("Hola Mundo, ahora en Python!")
